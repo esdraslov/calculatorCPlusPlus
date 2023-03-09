360007bdfa58cee7b09913951a0d07f23ea31545
Esdraslov <103470150+esdraslov@users.noreply.github.com>
2023-03-09 01:33:31 +0000

    first commit

--- .ccls-cache/@home@runner@calculatorCPP/main.cpp
+++ .ccls-cache/@home@runner@calculatorCPP/main.cpp
@@ -3,5 +3,49 @@
 using namespace std;
 
 int main() {
-  cout << "Hello World!\n";
+  int opt = 0;
+  int val1 = 0;
+  int val2 = 0;
+  int answer = 0;
+  int rest = 0;
+  cout << "choose a opition\n";
+  cout << "(1) addition\n";
+  cout << "(2) subtraction\n";
+  cout << "(3) multiplication\n";
+  cout << "(4) division\n";
+  cin >> opt;
+  if(opt == 1){
+    cout << "value 1: ";
+    cin >> val1;
+    cout << "value 2: ";
+    cin >> val2;
+    answer = val1 + val2;
+    cout << "answer is: " << answer << "\n";
+  }else if(opt == 2){
+    cout << "value 1: ";
+    cin >> val1;
+    cout << "value 2: ";
+    cin >> val2;
+    answer = val1 - val2;
+    cout << "answer is: " << answer << "\n";
+  }else if(opt == 3){
+    cout << "value 1: ";
+    cin >> val1;
+    cout << "value 2: ";
+    cin >> val2;
+    answer = val1 * val2;
+    cout << "answer is: " << answer << "\n";
+  }else if(opt == 4){
+    cout << "value 1: ";
+    cin >> val1;
+    cout << "value 2: ";
+    cin >> val2;
+    answer = val1 / val2;
+    rest = val1 % val2;
+    cout << "answer is: " << answer << "\n";
+    cout << "rest is: " << rest << "\n";
+  }else{
+    cout << opt << " is not a valid option\n";
+  }
+  return 0;
 }
